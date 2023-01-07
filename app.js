f792ce99c08c83d49aa115c34c36cc5f5cf2e327
Sparsh <sprshngm9@gmail.com>
2023-01-07 23:31:40 +0530

    Final Commit

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ if(process.env.NODE_ENV !== 'production'){
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const User=require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const secret = process.env.SECRET;
 const dbUrl = process.env.DB_URL;
-console.log(dbUrl);
-const MongoStore  = require('connect-mongo');
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
+    useCreateIndex:true,
     useUnifiedTopology: true
 });
 const db = mongoose.connection;
@@ -20,21 +19,21 @@ db.on("error", console.error.bind(console,"Connection error"));
 db.once("open",()=>{
     console.log("Database Connected");
 })
-const store= MongoStore.create({
-    mongoUrl:dbUrl,
-    crypto:{secret},
-    
-    touchAfter:24*60*60
-});
-store.on("error",function(e){
-    console.log("Session Store Error",e)
-})
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/signup', (req, res)=>{
+    res.render('signup');
+});
 
 app.post('/signup',async(req,res)=>{
     const {email, username, password} = req.body;
+    console.log(email, username, password);
     const user = new User({email, username, password});
+    console.log(user);
     await user.save();
+    res.send("Successfull registration");
 })
 
 app.post('/login',async(req,res)=>{
@@ -44,11 +43,11 @@ app.post('/login',async(req,res)=>{
         res.send("User Found");
     }
     else {
-        res.send("The username or password is incorrect");
+        res.send("The Email or Password is incorrect");
     }
     
 })
 
-app.listen(3000 , ()=>{
-    console.log("Listening on port 3000");
+app.listen(8000 , ()=>{
+    console.log("Listening on port 8000");
 })
--- models/user.js
+++ models/user.js
@@ -1,31 +1,35 @@
 const mongoose = require('mongoose');
+const bcrypt=require('bcrypt');
 const Schema = mongoose.Schema;
 
 const userSchema= new Schema({
     email:{
         type : String,
         unique  : true,
-        required : [true, 'Email cannot be blank']
+        // required : [true, 'Email cannot be blank']
     },
     username:{
         type : String,
         unique  : true,
-        required : [true, 'Username cannot be blank']
+        // required : [true, 'Username cannot be blank']
     },
     password:{
         type : String,
-        required : [true, 'Password cannot be blank']
+        // required : [true, 'Password cannot be blank']
     }
 })
-userSchema.statics.findAndValidate = async function(username, password) {
-    const foundUser= await this.findOne({username});
-    const isValid = await bcrypt.compare(password, foundUser.password);
-    return isValid ? foundUser : false;
+userSchema.statics.findAndValidate = async function(email, password) {
+    const foundUser= await this.findOne({email});
+    if(foundUser){
+        const isValid = await bcrypt.compare(password, foundUser.password);
+        return isValid ? foundUser : false;
+    }
+    else return false;
 }
 
 userSchema.pre('save',async function(next){
     if(!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password,12);
+    this.password = await bcrypt.hash(this.password, 12);
     next();
 })
 module.exports = mongoose.model('User',userSchema);
